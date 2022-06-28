da06859bf16ab87438756ca0fa5044ab8dbaf205
Abhimanyu Singh <abhimanyus1997@gmail.com>
2022-06-28 17:24:07 +0530

    Update my_modules.py

--- my_modules.py
+++ my_modules.py
@@ -6,7 +6,7 @@ logging.basicConfig(filename="day17_module.log",
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='w')
 
 # Creating an object
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 list_logger = logging.getLogger("list_Log")
 dict_logger = logging.getLogger("dict_Log")
 
@@ -20,42 +20,45 @@ logger.info("logger Ready")
 list_logger.info("list logger Ready")
 dict_logger.info("dict logger Ready")
 
+""" list_logger.debug("Harmless debug Message")
+list_logger.info("Just an information")
+list_logger.warning("Its a Warning")
+list_logger.error("Did you try to divide by zero")
+list_logger.critical("Internet is down") """
 
-## USER DEFINED LIST CLASS
+# USER DEFINED LIST CLASS
 try:
-    class mylist:
+    class MyList:
         try:
-            def __init__(self,mylist) -> None:
+            def __init__(self, mylist) -> None:
                 # Type checking of user input paramater for constructor
                 if type(mylist) != list:
-                    raise TypeError("Class paramater is not a List")
                     list_logger.warning(f"Class paramater passed is of type {type(mylist)}")
+                    raise TypeError("Class paramater is not a List")
                 self.mylist = mylist
                 list_logger.info(f"Created self.mylist as {self.mylist}")
         except Exception as e:
             list_logger.error(f"Error occured during Class initialization: {e}")
 
-
         try:
-            def append(self,element) -> None:
+            def append(self, element) -> None:
                 """Adds an element at the end of the list"""
                 try:
                     list_logger.info(f"Using append method with parameter: element={element}")
                     return self.mylist.append(element)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside Append Method: {e}")
+                except Exception as err:
+                    list_logger.error(f"Error occured at Append Method: {err}")
         except Exception as e:
-            list_logger.error(f"Error occured at Append Method: {e}")
-
+            list_logger.error(f"Error occured during Class initialization: {e}")
 
         try:
             def clear(self) -> None:
                 """Removes all the elements from a list"""
                 try:
                     list_logger.info(f"Using clear method")
-                    return self.mylist.clear()
-                except Exception as e:
-                    list_logger.error(f"Error occured inside clear Method: {e}")
+                    return self.myList.clear()
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside clear Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at clear Method: {e}")
 
@@ -64,112 +67,98 @@ try:
                 """Returns a copy of the specified list."""
                 try:
                     list_logger.info(f"Using copy method")
-                    return self.mylist.copy()
-                except Exception as e:
-                    list_logger.error(f"Error occured inside copy Method: {e}")
+                    return self.myList.copy()
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside copy Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at copy Method: {e}")
 
-
         try:
-            def count(self,value) -> int:
+            def count(self, value) -> int:
                 """Returns the number of elements with the specified value"""
                 try:
                     list_logger.info(f"Using count method with parameter: value={value}")
-                    return self.mylist.count(value)
-                except Exception as e:
+                    return self.myList.count(value)
+                except Exception as exp:
                     list_logger.error(
-                        f"Error occured inside count Method: {e}")
+                        f"Error occured inside count Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at count Method: {e}")
 
-
         try:
-            def extend(self,iterable) -> list:
+            def extend(self, iterable) -> list:
                 """Add the elements of a list (or any iterable), to the end of the current list"""
                 try:
                     list_logger.info(f"Using extend method with parameter: iterable={iterable}")
-                    return self.mylist.extend(iterable)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside extend Method: {e}")
+                    return self.myList.extend(iterable)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside extend Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at extend Method: {e}")
 
-
-
         try:
-            def index(self,elmnt) -> int:
+            def index(self, elmnt) -> int:
                 """Returns the index of the first element with the specified value"""
                 try:
                     list_logger.info(f"Using index method with parameter: element={elmnt}")
-                    return self.mylist.index(elmnt)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside index Method: {e}")
+                    return self.myList.index(elmnt)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside index Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at index Method: {e}")
 
-
-
         try:
-            def insert(self,pos, elmnt) -> list:
+            def insert(self, pos, elmnt) -> list:
                 """Adds an element at the specified position"""
                 try:
                     list_logger.info(f"Using insert method with parameters: position={pos} & element={elmnt}")
-                    return self.mylist.insert(pos, elmnt)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside insert Method: {e}")
+                    return self.myList.insert(pos, elmnt)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside insert Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at insert Method: {e}")
 
-
-
         try:
-            def pop(self,pos = -1):
+            def pop(self, pos=-1):
                 """Removes the element at the specified position"""
                 try:
                     list_logger.info(f"Using pop method with parameter: position={pos}")
-                    return self.mylist.pop(pos)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside pop Method: {e}")
+                    return self.myList.pop(pos)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside pop Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at pop Method: {e}")
 
-
-
         try:
-            def remove(self,elmnt) -> None:
+            def remove(self, elmnt) -> None:
                 """Removes the first item with the specified value"""
-               try:
+                try:
                     list_logger.info(f"Using remove method with parameter: element={elmnt}")
-                    return self.mylist.remove(elmnt)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside remove Method: {e}")
+                    return self.myList.remove(elmnt)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside remove Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at remove Method: {e}")
-
-
 
         try:
             def reverse(self) -> list:
                 """Reverses the order of the list"""
                 try:
                     list_logger.info(f"Using reverse method")
-                    return self.mylist.reverse()
-                except Exception as e:
-                    list_logger.error(f"Error occured inside reverse Method: {e}")
+                    return self.myList.reverse()
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside reverse Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at reverse Method: {e}")
 
-
-
         try:
-            def sort(self,is_reverse=False) -> list:
+            def sort(self, is_reverse=False) -> list:
                 """Sorts the list"""
-               try:
+                try:
                     list_logger.info(f"Using sort method with parameter: reverse={is_reverse}")
-                    return self.mylist.sort(reverse=is_reverse)
-                except Exception as e:
-                    list_logger.error(f"Error occured inside sort Method: {e}")
+                    return self.myList.sort(reverse=is_reverse)
+                except Exception as exp:
+                    list_logger.error(f"Error occured inside sort Method: {exp}")
         except Exception as e:
             list_logger.error(f"Error occured at sort Method: {e}")
 
